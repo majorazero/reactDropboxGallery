c3bf5a72be0993a4350b79175aec42d402c213fd
majorazero <daniel.cw.hsu@gmail.com>
2018-03-24 20:21:58 -0700

    added more styling

--- public/component.js
+++ public/component.js
@@ -2,7 +2,37 @@ class Page extends React.Component{
   render(){
     return(
       <div className="woop">
-        Hello World from REACT!
+        <Gallery />
+      </div>
+    );
+  }
+  _dropboxAPIFetch(){
+    let key = "i2ucyknubgl3g21";
+    let secret = "m555a1swkhbs6jf";
+    let token = "4DOPxV7FGysAAAAAAAAMpqlYCZBq15TA4_Ws9KtFLEZdiQLyeOQZ9j2xUSibZ7nC";
+  }
+}
+class Gallery extends React.Component{
+  render(){
+    return(
+      <div className="gallery">
+        Woops.
+        <Portrait />
+      </div>
+    );
+  }
+}
+class Portrait extends React.Component{
+  render(){
+    return(
+      <div>
+        <div className="portrait">
+          Hey.
+        </div>
+        <div className="portrait">
+          Hey.
+        </div>
+
       </div>
     );
   }
