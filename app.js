cc92adb0dbfdbe42687f35548fa95feb494cc498
majorazero <daniel.cw.hsu@gmail.com>
2018-03-30 16:30:54 -0700

    switched atob module to btoa module

--- app.js
+++ app.js
@@ -4,9 +4,16 @@ let app = express();
 require('isomorphic-fetch');
 let dropbox = require("dropbox").Dropbox;
 let dbx = new dropbox({accessToken: "4DOPxV7FGysAAAAAAAAMp_xQ3qo3vmNYzHJzRAuRnCmQPWmANT9-H2oOkgr_PJkX"});
-global.atob = require("atob");
+global.btoa = require("btoa");
 
-app.use(express.static("public"));
+app.get("https://content.dropboxapi.com/1/thumbnails/auto/dude1.png?size=s&access_token=4DOPxV7FGysAAAAAAAAMp_xQ3qo3vmNYzHJzRAuRnCmQPWmANT9-H2oOkgr_PJkX",function(req,res){
+  console.log(res);
+});
+
+fetch("https://content.dropboxapi.com/1/thumbnails/auto/dude1.png?size=s&access_token=4DOPxV7FGysAAAAAAAAMp_xQ3qo3vmNYzHJzRAuRnCmQPWmANT9-H2oOkgr_PJkX",function(req,res){
+  console.log(res);
+});
+//app.use(express.static("public"));
 
 // dbx.filesListFolder({path:""})
 //   .then(function(response){
@@ -24,19 +31,6 @@ app.use(express.static("public"));
 //     console.log(error);
 //   });
 
-app.get("/",function(req,response){
-  //res.send("Hello World");
-  //res.send(img);
-  dbx.filesGetThumbnail({path:"/dude1.png"})
-    .then(function(res){
-      var img = document.createElement("img");
-      img.src = "data:image/jpeg;base64" + atob(res.fileBinary);
-      document.body.appendChild(img);
-      //response.send(atob(res.fileBinary));
-    })
-    .catch(function(error){
-      console.log(error);
-    });
-});
+
 
 module.exports = app;
