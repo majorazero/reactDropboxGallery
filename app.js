45121c07e107244663584d0f7dcf721cbaad3232
majorazero <daniel.cw.hsu@gmail.com>
2018-04-14 22:05:02 -0700

    removed unneccesary chunk of code

--- app.js
+++ app.js
@@ -13,24 +13,5 @@ app.get("https://content.dropboxapi.com/1/thumbnails/auto/dude1.png?size=s&acces
 fetch("https://content.dropboxapi.com/1/thumbnails/auto/dude1.png?size=s&access_token=4DOPxV7FGysAAAAAAAAMp_xQ3qo3vmNYzHJzRAuRnCmQPWmANT9-H2oOkgr_PJkX",function(req,res){
   console.log(res);
 });
-//app.use(express.static("public"));
-
-// dbx.filesListFolder({path:""})
-//   .then(function(response){
-//     console.log(response.entries);
-//   })
-//   .catch(function(error){
-//     console.log(error);
-//   });
-// dbx.filesGetThumbnail({path:"/dude1.png"})
-//   .then(function(res){
-//     console.log(res);
-//     var img = atob(res.fileBinary);
-//   })
-//   .catch(function(error){
-//     console.log(error);
-//   });
-
-
 
 module.exports = app;
